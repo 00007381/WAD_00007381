181a1cccae782f1ac61f24160d88f7b63e111444
00007381 <sardor20000707@gmail.com>
2021-03-12 08:55:50 +0500

    Structured models

---
+++ API/API/Models.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Show
+    {
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime ReleasedDate { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string PhotoURL { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Range(0.0, 10)]
+        public double Rating { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int NumOfEpisodes { get; set; }
+
+        public virtual ICollection<Character> Characters { get; set; }
+    }
+
+    public class Character
+    {
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Nickname { get; set; }
+
+        [Required]
+        public string Bio { get; set; }
+
+        [Required]
+        public string Appearance { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string PhotoURL { get; set; }
+
+        [MinLength(2)]
+        public string Catchphrase { get; set; }        
+
+        [Range(0, int.MaxValue)]
+        public int Age { get; set; }
+
+        public int ShowId { get; set; }
+        public virtual Show Show { get; set; }
+    }
+}
